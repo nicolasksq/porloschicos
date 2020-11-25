02e3c966dc94549fc59a0e5d85533afbfd6873df
dafnemus <dafnemus@gmail.com>
2020-11-24 21:06:03 -0300

    pantalla de registro

--- src/screens/Login/components/Register/index.js
+++ src/screens/Login/components/Register/index.js
@@ -13,15 +13,11 @@ import Button from '../Button';
 
 import styles from './index.module.scss';
 
-function FormLogin() {
+function Register() {
   return (
     <>
-      <p className={styles.signUp}>
-        ¿No tenés cuenta?
-        <span>Registrate</span>
-      </p>
       <Title upperTitle="¡Bienvenido!">
-        Iniciar Sesión
+        Registrate
       </Title>
       <form className="form" action="">
         <div className={styles.containerInput}>
@@ -40,18 +36,24 @@ function FormLogin() {
             icon={passIcon}
             iconActive={passIconActive}
           />
-          <Button variantStyle="anchor">
-            ¿Olvidaste la contraseña?
-          </Button>
+        </div>
+        <div className={styles.containerInput}>
+          <Input
+            htmlFor="e-mail"
+            label="E-mail"
+            icon={passIcon}
+            iconActive={passIconActive}
+          />
+        </div>
+        <div className={styles.containerInput}>
+          <Input
+            htmlFor="dni"
+            label="Documento"
+            icon={passIcon}
+            iconActive={passIconActive}
+          />
         </div>
         <div className={styles.containerButtonCheckbox}>
-          <div className={styles.containerCheckbox}>
-            <Input
-              htmlFor="rememberPass"
-              type="checkbox"
-              label="Recordar Contraseña"
-            />
-          </div>
           <Button>Iniciar Sesión</Button>
         </div>
       </form>
@@ -59,16 +61,16 @@ function FormLogin() {
         <Button variantStyle="light">
           <img className={styles.iconBtn} src={fbIcon} alt="" />
           {' '}
-          Continuar con Facebook
+          Registrate con Facebook
         </Button>
         <Button variantStyle="light">
           <img className={styles.iconBtn} src={googleIcon} alt="" />
           {' '}
-          Continuar con Google
+          Registrate con Google
         </Button>
       </div>
     </>
   );
 }
 
-export default FormLogin;
+export default Register;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import ilustration from './assets/ilustration.svg';
 // import FormLogin from './components/FormLogin';
-import RestorePassword from './components/restorePassword';
+// import RestorePassword from './components/RestorePassword';
+import Register from './components/Register';
 import styles from './index.module.scss';
 
 function Login() {
@@ -13,7 +14,7 @@ function Login() {
         <img className={styles.ilustration} src={ilustration} alt="Ilustracion" />
       </div>
       <div className={styles.containerForm}>
-        <RestorePassword />
+        <Register />
       </div>
     </div>
   );
