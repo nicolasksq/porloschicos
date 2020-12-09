f156efa84422743aa25fcf0b66f2b2753fc56067
dafnemus <dafnemus@gmail.com>
2020-12-08 22:15:43 -0300

    configuracion service api

--- src/service/reproService.js
+++ src/service/reproService.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 import api from './api';
 
-export const postDataUser = () => api.post('/api/authenticate');
+export const postDataUser = (data) => api.post('/api/authenticate', data);
